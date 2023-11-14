e312efbd2c046cde7bcd53279c341c06cd437d28
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-11-14 12:02:32 +0300

    The product partition that is not on the device has been removed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,6 @@ TARGET_BOARD_PLATFORM := mt6739
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt8765
